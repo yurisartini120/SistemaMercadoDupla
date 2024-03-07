3772c18cda067e7db51ee699a7ac2dd182190fb5
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-03-07 15:58:23 -0300

    Sistema finalizado
    Terminamos o sistemas, e fizemos os testes automaticos

--- sistema feito, antes do unitTest - Versão para modificar/teste.py
+++ sistema feito, antes do unitTest - Versão para modificar/teste.py
@@ -1,56 +1,36 @@
-<<<<<<< HEAD
-import os
-import time
+import unittest
+import mercado
 
-class Carrinho:
-    listaProdutos = []
-    listaPrecos = []
-    listaQuantidade = []
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.carrinho = mercado.Carrinho()
+        
+    def test_adicionar(self):
+        self.carrinho.adicionarProduto(produtoNome="arroz", produtoPreco=-2, quantidadeProdutos=2)
+        
+    #     self.assertEqual(1, len(self.carrinho.listaPrecos))
+    #     self.assertEqual(1, len(self.carrinho.listaProdutos))   
+    #     self.assertEqual(1, len(self.carrinho.listaQuantidade)) 
+        
+    # def test_remover(self):
+    #     self.carrinho.adicionarProduto(produtoNome="arroz", produtoPreco=10, quantidadeProdutos=2)
+    #     self.carrinho.removerProduto(numeroItem=0)
+
+    #     self.assertEqual(0, len(self.carrinho.listaPrecos))
+    #     self.assertEqual(0, len(self.carrinho.listaProdutos))   
+    #     self.assertEqual(0, len(self.carrinho.listaQuantidade)) 
+     
+     
+    # def test_calcular(self):
+    #     self.carrinho.adicionarProduto(produtoNome="arroz", produtoPreco=5, quantidadeProdutos=4)
+    #     self.carrinho.adicionarProduto(produtoNome="feijao", produtoPreco=6, quantidadeProdutos=3)
+    #     self.carrinho.adicionarProduto(produtoNome="carne", produtoPreco=7, quantidadeProdutos=2)
+        
+    #     valorTotal = self.carrinho.calcularValorTotal()
+    #     self.assertEqual(52, valorTotal)
+
     
 
-produtoNome = input("Digite o nome do produto: ")
-produtoPreco = float(input("Digite o preço do produto: "))
-quantidadeProdutos = int(input("Digite a quantidade de produtos: "))
-
-def Adicionar():
-    
-    print("Coloque um valor válido! \n")
-    Carrinho.listaProdutos.append(produtoNome)
-    Carrinho.listaPrecos.append(produtoPreco)
-    Carrinho.listaQuantidade.append(quantidadeProdutos)
-
-
-
-print(Adicionar())
-
-print(Carrinho.listaProdutos)
-print(Carrinho.listaPrecos)
-=======
-import os
-import time
-
-class Carrinho:
-    listaProdutos = []
-    listaPrecos = []
-    listaQuantidade = []
-    
-
-produtoNome = input("Digite o nome do produto: ")
-produtoPreco = float(input("Digite o preço do produto: "))
-quantidadeProdutos = int(input("Digite a quantidade de produtos: "))
-
-def Adicionar():
-    
-    print("Coloque um valor válido! \n")
-    Carrinho.listaProdutos.append(produtoNome)
-    Carrinho.listaPrecos.append(produtoPreco)
-    Carrinho.listaQuantidade.append(quantidadeProdutos)
-
-
-
-print(Adicionar())
-
-print(Carrinho.listaProdutos)
-print(Carrinho.listaPrecos)
->>>>>>> 9b894a910dc158995df5fb0a3eea6cc317f3c741
-print(Carrinho.listaQuantidade)
+if __name__ == '__main__':
+    unittest.main()
