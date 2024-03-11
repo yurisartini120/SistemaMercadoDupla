f586aa7c76a10508532f5e75fdc0365e9d722e8d
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-03-11 16:01:42 -0300

    Update tarefaTestesKaua.py

--- tabelaFutebol/tarefaTestesKaua.py
+++ tabelaFutebol/tarefaTestesKaua.py
@@ -1,3 +1,17 @@
+import mysql.connector as mysql
+
+#use para conectar (o banco de dados ja tem q estar criado)
+database = input("Digite um database que você deseja conectar: ")
+
+
+conexao = mysql.connect(
+host ='127.0.0.1',
+port=3306,
+database = database,
+user = 'root',
+password=''
+)
+
 class time:
         def __init__(self, nome, quantidadePontos):
             self.nome = nome
@@ -11,8 +25,7 @@ quantidadePontos = int(input("Quantidade de pontos: "))
 
 flamengo = time(nome=nome, quantidadePontos=quantidadePontos)
 
-print(self.flamengo.nome)
-print(self.flamengo.quantidadePontos)
+
 
 
 
