f47040bfccf0490865d91f97c72bd4125f8972dc
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-03-11 15:40:16 -0300

    tarefa de testes para lembrar os comandos

---
+++ tabelaFutebol/tarefaTestesKaua.py
@@ -0,0 +1,18 @@
+class time:
+        def __init__(self, nome, quantidadePontos):
+            self.nome = nome
+            self.quantidadePontos = quantidadePontos
+
+            
+
+
+nome = input("Nome do time:")
+quantidadePontos = int(input("Quantidade de pontos: "))
+
+flamengo = time(nome=nome, quantidadePontos=quantidadePontos)
+
+print(self.flamengo.nome)
+print(self.flamengo.quantidadePontos)
+
+
+
