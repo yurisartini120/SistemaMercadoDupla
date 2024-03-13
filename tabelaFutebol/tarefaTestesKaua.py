5c80ea17d34960e1922dcab67e3042fb58fc8f92
Kaua Fonseca <137219893+KauaFonsecaDeCarvalho@users.noreply.github.com>
2024-03-13 16:50:15 -0300

    tentativa de mostrar os resultados
    deu certo

--- tabelaFutebol/tarefaTestesKaua.py
+++ tabelaFutebol/tarefaTestesKaua.py
@@ -1,4 +1,65 @@
 import mysql.connector as mysql
+import time 
+import random
+
+
+class time:
+        def __init__(self, nome):
+            self.nome = nome
+            
+class TabelaCampeonato:
+    def criarTabela():
+        cursor.execute("Create table if not exists  `teste2` (`id` int not null auto_increment primary key,`nome` varchar(255) not null, `partidas` int, `V` int, `D` int, `E` int, `SG` int, `pontos` int);")
+        
+    # def adicionarTime():
+    #     contador = 0
+    #     while contador != 8:
+    #         timeNome = input("Digite o nome do time: ")
+    #         cursor.execute("insert into teste2 (nome, partidas, V, D, E, SG, pontos) VALUES (%s, %s, %s, %s, %s, %s, %s);", (timeNome , 0, 0, 0, 0, 0, 0))
+            
+    #         contador+=1
+    #         conexao.commit()
+
+
+selecionar = "Select * From teste2"
+          
+            
+# class jogo:
+    
+#     timeCasa = input("time da casa:")
+#     cursor.execute()
+#     timeDesafiante = input("time desafiante:")
+#     pontos = 0
+   
+#     golsCasa = 0
+#     golsDesafiante = 0
+
+#     time.sleep(0.5)
+#     print("------------------------------------------")
+#     print(golsCasa, "X", golsDesafiante)
+#     time.sleep(0.5)
+    
+#     if (golsCasa > golsDesafiante):
+#         print(timeCasa, "Vencedor com ",golsCasa, "GOLS")
+#         pontos += 3
+#         print("pontuação: +", pontos)
+        
+#     elif(golsCasa == golsDesafiante):
+#         print("Empate!")
+#         pontos += 1
+#         print("pontuação para ambos: +", pontos)
+        
+#     elif(golsDesafiante > golsCasa):
+#         print(timeDesafiante, "Vencedor com ",golsDesafiante, "GOLS")
+#         pontos += 3
+#         print("pontuação: +", pontos)
+#     print("------------------------------------------")
+        
+        
+
+    
+    
+    
 
 #use para conectar (o banco de dados ja tem q estar criado)
 database = "teste"
@@ -14,14 +75,21 @@ password=''
 
 cursor = conexao.cursor()
 
-cursor.execute("Create table if not exists  `teste` (`id` int not null auto_increment primary key,`nome` varchar(255) not null, `pontos` int);")
+
+TabelaCampeonato.criarTabela()
+
+# TabelaCampeonato.adicionarTime()
+
+cursor.execute(selecionar)
+resultado = cursor.fetchall()
+print(resultado)
+conexao.commit()
 
 
 
-# class time:
-#         def __init__(self, nome, quantidadePontos):
-#             self.nome = nome
-#             self.quantidadePontos = quantidadePontos
+
+
+
 
             
 
--- tabelaFutebol/teste.py
+++ tabelaFutebol/teste.py
@@ -1,1 +1,17 @@
-teste = input("teste")
+
+i = 1
+
+tabela = {
+    "Flamengo": 85,
+    "São paulo": 30,
+    "Palmeiras": 84,
+    "Avaí": 100
+}
+
+print("Time " + "Pontos")
+for i in sorted(tabela, key = tabela.get, reverse=True):
+    print(i, tabela[i])
+
+
+
+
